b02a73a9b6aa372e28cbf5bbe3d4b6cc35a9aa60
BalDiego <diegobalt16@gmail.com>
2026-06-09 17:46:29 -0600

    Initial Commit

---
+++ PasteleriaSofia/PasteleriaSofiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasteleriaSofiaApp.swift
+//  PasteleriaSofia
+//
+//  Created by Diego Baltodano on 9/6/26.
+//
+
+import SwiftUI
+
+@main
+struct PasteleriaSofiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PasteleriaSofiaTests/PasteleriaSofiaTests.swift
@@ -0,0 +1,19 @@
+//
+//  PasteleriaSofiaTests.swift
+//  PasteleriaSofiaTests
+//
+//  Created by Diego Baltodano on 9/6/26.
+//
+
+import Testing
+@testable import PasteleriaSofia
+
+struct PasteleriaSofiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
